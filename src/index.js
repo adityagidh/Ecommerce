0055f1c14672a932fdf859f95693c11785037728
adityagidh <40241385+adityagidh@users.noreply.github.com>
2023-04-01 17:33:16 +0530

    Update index.js
    This will solve the error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Rout from "./Routes"
 import ReactDOM from 'react-dom/client'
 
-// ReactDOM.render(<Rout/>,document.getElementById("root"))
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Rout />
-  </React.StrictMode>
-);
+//This will fix the issue bro
+ReactDOM.render(<Rout/>,document.getElementById("root"))
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Rout />
+//   </React.StrictMode>
+// );
